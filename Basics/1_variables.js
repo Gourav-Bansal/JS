4e6580f041f52f8211b65bb3b3df855fac277c10
Gourav Bansal <75083190+Gourav-Bansal@users.noreply.github.com>
2024-11-06 19:00:34 +0000

    Config done

---
+++ Basics/1_variables.js
@@ -0,0 +1,14 @@
+const accountID = 12345
+let accountEmail = "gourav@gmail"
+var accountPassword = "9876"
+accountCity = "Hisar"
+
+// accountID = 67890    "Constant value can't be changed"
+accountEmail = "bansal@gmail"
+accountPassword = "0000"
+accountCity = "Delhi"
+
+// Prefer not to use var
+//   because of issue in block scope and function scope
+console.log(accountID)
+console.table([accountID, accountEmail, accountPassword, accountCity])
