35a1c8b4c270a65862d7c4d9ec65a6bfa1e4624b
Gourav Bansal <75083190+Gourav-Bansal@users.noreply.github.com>
2024-12-23 09:30:41 +0000

    config done

---
+++ Basics/2_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all JS code as newer version
+
+//  alert(3+3)     // we are using node Js and not browser
+ console.log(3+3)
+
+ /*  data types
+             number range 2 to power 53
+             bigint
+             string
+             boolean ---- true/false
+             null ----  standalone value
+             undefined
+             symbol ---  to find uniqueness
+
+
+             object
+             */
+
+console.log(typeof"Gourav")   // to find type of the enered value
+
+console.log(typeof undefined)  //  output undefined
+console.log(typeof null)       //  output object  
