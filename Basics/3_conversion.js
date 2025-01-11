b4a2e2197bc8bfccd12ece11d71392bc5f99077f
Gourav Bansal <75083190+Gourav-Bansal@users.noreply.github.com>
2025-01-11 08:07:18 +0000

    config done

---
+++ Basics/3_conversion.js
@@ -0,0 +1,28 @@
+/* let score = 33
+let score_1 = "32"
+
+console.log(typeof score)
+console.log(typeof(score_1))
+
+let valueInNumber = Number(score_1)
+console.log(typeof(valueInNumber)) */
+
+let score = "33abc"
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+/* "33" =>  33
+   "33abc"  => NaN
+    true => 1     false => 0
+*/
+
+let isLogIn = 1
+let booleanisLogIn = Boolean(isLogIn)
+console.log(booleanisLogIn);
+
+// 1 => true  0 => false
+// "" => false
+// "gourav" => true
+
+
