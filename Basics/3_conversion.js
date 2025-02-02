7f1bf37d36e245fffd5f4bb9da9fbc221cab3564
Gourav Bansal <75083190+Gourav-Bansal@users.noreply.github.com>
2025-02-02 11:08:03 +0000

    Config done

--- Basics/3_conversion.js
+++ Basics/3_conversion.js
@@ -1,28 +1,46 @@
-/* let score = 33
-let score_1 = "32"
+// let score = 33
+// let score_1 = "32"
 
-console.log(typeof score)
-console.log(typeof(score_1))
+// console.log(typeof score)
+// console.log(typeof(score_1))
 
-let valueInNumber = Number(score_1)
-console.log(typeof(valueInNumber)) */
+// let valueInNumber = Number(score_1)
+// console.log(typeof(valueInNumber)) 
 
-let score = "33abc"
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// let score = "33abc"
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 /* "33" =>  33
    "33abc"  => NaN
     true => 1     false => 0
 */
 
-let isLogIn = 1
-let booleanisLogIn = Boolean(isLogIn)
-console.log(booleanisLogIn);
+// let isLogIn = 1
+// let booleanisLogIn = Boolean(isLogIn)
+// console.log(booleanisLogIn);
 
 // 1 => true  0 => false
 // "" => false
 // "gourav" => true
 
+//       *********OPERATIONS*************
+let value = 1
+let negValue = -value
+// console.log(negValue);
+
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5**5);
+
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2);
+// console.log(1 + 3 + "2");   //   first do calculation and then print as string
+// console.log("1" + 2 + 3);   //since first char is string it treat whole as string
+
+// console.log(true);
+// console.log(+true);
 
---
+++ Basics/4_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2==1);
+
+// console.log("2" > 1);   //   JS converts "2" data type
+
+//  NOTE : in js == and other comparison operatore work differentlly
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >=0);  In this null is converted into 0
+
+// === is strict check i.e. it also checks the data types along with values
+
+console.log("2" === 2);
+
